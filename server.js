3bd7dfba3121b0de86d7d16520b8da9338c07524
Rodika <raryku@yahoo.com>
2023-04-20 17:26:24 -0700

    modified

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const booksRoute = require('./routes/FetchBooks');
+const booksRoute = require('./router/FetchBooks');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
